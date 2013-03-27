cbe25bfc9cac87e2f8827d1f8df3354a1e7814a0
Michael Marinetti <marinett@adriatic.cse.msu.edu>
2013-03-27 15:45:37 -0400

    save-load

--- make-html.py
+++ make-html.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python
 
 import os
+import sys
 from drinkz import recipes, db
+from drinkz.db import load_db
 
 try:
     os.mkdir('html')
@@ -9,29 +11,37 @@ except OSError:
     # already exists
     pass
 
-db._reset_db()
+def main(args):
+    filename = args[1]
 
-db.add_bottle_type('Johnnie Walker', 'black label', 'blended scotch')
-db.add_to_inventory('Johnnie Walker', 'black label', '500 ml')
+    load_db(filename)
 
-db.add_bottle_type('Uncle Herman\'s', 'moonshine', 'blended scotch')
-db.add_to_inventory('Uncle Herman\'s', 'moonshine', '5 liter')
+if __name__ == '__main__':
+    main(sys.argv)
 
-db.add_bottle_type('Gray Goose', 'vodka', 'unflavored vodka')
-db.add_to_inventory('Gray Goose', 'vodka', '1 liter')
+#db._reset_db()
 
-db.add_bottle_type('Rossi', 'extra dry vermouth', 'vermouth')
-db.add_to_inventory('Rossi', 'extra dry vermouth', '24 oz')
+#db.add_bottle_type('Johnnie Walker', 'black label', 'blended scotch')
+#db.add_to_inventory('Johnnie Walker', 'black label', '500 ml')
 
-r = recipes.Recipe('vodka martini', [('unflavored vodka', '6 oz'), ('vermouth', '1.5 oz')])
-db.add_recipe(r)
+#db.add_bottle_type('Uncle Herman\'s', 'moonshine', 'blended scotch')
+#db.add_to_inventory('Uncle Herman\'s', 'moonshine', '5 liter')
 
-r2 = recipes.Recipe('scotch on the rocks', [('blended scotch', '4 oz')])
-db.add_recipe(r2)
+#db.add_bottle_type('Gray Goose', 'vodka', 'unflavored vodka')
+#db.add_to_inventory('Gray Goose', 'vodka', '1 liter')
 
-r3 = recipes.Recipe('vomit inducing martini', [('orange juice', '6 oz'), ('vermouth',
-                                                                          '1.5 oz')])
-db.add_recipe(r3)
+#db.add_bottle_type('Rossi', 'extra dry vermouth', 'vermouth')
+#db.add_to_inventory('Rossi', 'extra dry vermouth', '24 oz')
+
+#r = recipes.Recipe('vodka martini', [('unflavored vodka', '6 oz'), ('vermouth', '1.5 oz')])
+#db.add_recipe(r)
+
+#r2 = recipes.Recipe('scotch on the rocks', [('blended scotch', '4 oz')])
+#db.add_recipe(r2)
+
+#r3 = recipes.Recipe('vomit inducing martini', [('orange juice', '6 oz'), ('vermouth',
+#                                                                          '1.5 oz')])
+#db.add_recipe(r3)
 
 
 fp = open('html/index.html', 'w')
